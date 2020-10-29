6eda3726c8326f18dabf885201bb1abc8fd4d923
Nghia.Bui <buitrungnghia1.11@gmail.com>
2020-10-29 10:24:51 +0700

    added truong hop khong nam trong mang b2

--- (b2)Mảng/array.js
+++ (b2)Mảng/array.js
@@ -8,6 +8,7 @@ class Arr{
                 return i;
             }
         }
+        return `${element} khong nam trong mang`;
     }
     enterArray(arr){
         this.array = arr;
@@ -15,5 +16,7 @@ class Arr{
 }
 let arr =  new Arr();
 arr.enterArray([1, 2, 3, 4]);
-let index = arr.findIndex(1);
-console.log(index);
+let index1 = arr.findIndex(1);
+let index2 = arr.findIndex(0);
+console.log(index1);
+console.log(index2);
