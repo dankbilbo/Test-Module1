ae91c7c8597cc7ddc9ab95f75845c18a5f5ef48d
Nghia.Bui <buitrungnghia1.11@gmail.com>
2020-10-29 11:08:33 +0700

    added alert

--- (b3)check nguyên âm/StringClass.js
+++ (b3)check nguyên âm/StringClass.js
@@ -16,6 +16,9 @@ class String{
         }
         return count;
     }
+    getString(){
+        return this.str;
+    }
     setString(str){
         this.str = str;
     }
@@ -24,3 +27,7 @@ let str = new String();
 str.setString("nghia");
 let num = str.check(["a", "o", "e", "u", "i"]);
 console.log(num);
+alert(`so ki tu nguyen am cua chuoi "${str.getString()}" la ${num}`);
+str.setString("java core");
+num = str.check(["a", "o", "e", "u", "i"]);
+alert(`so ki tu nguyen am cua chuoi "${str.getString()}" la ${num}`);
